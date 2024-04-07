815cbb9920c03095405698e95493b5355e17b1fa
oneplusossadmin <oneplusossadmin@oneplus.com>
2024-04-07 17:50:58 +0800

    Synchronize code for OnePlus CPH2573_14.0.0.604(EX01) CPH2583_14.0.0.604(EX01)

--- vendor/qcom/opensource/camera-kernel/drivers/cam_cpas/cam_cpas_hw.c
+++ vendor/qcom/opensource/camera-kernel/drivers/cam_cpas/cam_cpas_hw.c
@@ -4615,6 +4615,8 @@ int cam_cpas_hw_probe(struct platform_device *pdev,
 	cpas_core->ahb_bus_scaling_disable = false;
 	cpas_core->full_state_dump = false;
 	cpas_core->smart_qos_dump = false;
+	cpas_core->force_hlos_drv = true;
+	cpas_core->force_cesta_sw_client = true;
 
 	atomic64_set(&cpas_core->monitor_head, -1);
 
